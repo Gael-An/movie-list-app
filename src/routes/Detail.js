710e3cb787bbc03a5a77bc3b539c0a159b835729
bnetcoder <silr01@naver.com>
2023-01-31 16:36:38 +0900

    CSS 스타일 적용

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -19,12 +19,20 @@ function Detail() {
 
   return (
     <div>
-      <h1>ID: {movie.id}</h1>
-      <h2>제목: {movie.title_long}</h2>
-      <h3>평점: {movie.rating}</h3>
-      <p>업로드 날짜: {movie.date_uploaded}</p>
-      <p>다운로드 횟수: {movie.download_count}</p>
-      <img src={movie.large_cover_image}></img>
+      {loading ? (
+        <div>
+          <h1>loading...</h1>
+        </div>
+      ) : (
+        <div>
+          <h1>ID: {movie.id}</h1>
+          <h2>제목: {movie.title_long}</h2>
+          <h3>평점: {movie.rating}</h3>
+          <p>업로드 날짜: {movie.date_uploaded}</p>
+          <p>다운로드 횟수: {movie.download_count}</p>
+          <img src={movie.large_cover_image}></img>
+        </div>
+      )}
     </div>
   );
 }
