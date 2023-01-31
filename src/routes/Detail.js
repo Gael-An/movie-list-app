151a80094fcf7e9cb61b36a9aef6529b640551da
bnetcoder <silr01@naver.com>
2023-01-31 16:25:31 +0900

    영화 디테일 페이지 정보 변경

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,17 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 function Detail() {
+  const [loading, setLoading] = useState(true);
   const { id } = useParams();
+  const [movie, setMovie] = useState([]);
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
+    setMovie(json.data.movie);
+    setLoading(false);
+    console.log(json);
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>Detail</h1>;
+
+  return (
+    <div>
+      <h1>ID: {movie.id}</h1>
+      <h2>제목: {movie.title_long}</h2>
+      <h3>평점: {movie.rating}</h3>
+      <p>업로드 날짜: {movie.date_uploaded}</p>
+      <p>다운로드 횟수: {movie.download_count}</p>
+      <img src={movie.large_cover_image}></img>
+    </div>
+  );
 }
 
 export default Detail;
